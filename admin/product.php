109add0172c568329a2b2316dd51d4fc4906e597
ptuyetanh <phamtuyetanh2@gmail.com>
2022-03-06 23:34:38 +0700

    add product.php fix index.php(admin)

--- admin/product.php
+++ admin/product.php
@@ -2,19 +2,19 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="./css/style.css">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
-  <title>Document</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>admin</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
 </head>
 
 <body>
-   <!-- header -->
-   <div class="container-fluid">
+    <!-- header -->
+    <div class="container-fluid">
         <div class="col-md-12">
             <nav class="navbar navbar-expand-lg navbar-light fixed-top" style="background-color:rgb(255, 145, 0)">
                 <div class="container-fluid">
@@ -62,33 +62,56 @@
             </nav>
         </div>
     </div>
-    <main>
+    <!--main -->
+    <main class="main_product">
+        <h2 class="text-center">Thêm sản phẩm</h2>
         <div class="container">
-          <h3 class="text-center text-primary product" >Thông tin sản phẩm</h3>
-          <div>
-            <a class="btn pe-3 ps-3 text-light" href="product.php" style="background-color:rgb(255, 145, 0)">Thêm</a>
-          </div>
-          <table class="table">
-            <thead>
-              <tr>
-                <th scope="col">Stt</th>
-                <th scope="col">Tên món ăn</th>
-                <th scope="col">Hình ảnh</th>
-                <th scope="col">Gía</th>
-                <th scope="col">Sửa</th>
-                <th scope="col">Xóa</th>
-              </tr>
-            </thead>
-          </table>
+            <div class="row">
+                <div class="col-md-4">
+                    <img src="./img/no-image.jpg" class="img-fluid mt-5" alt="...">
+
+                </div>
+                <div class="col-md-8">
+                    <form class="product_input mt-5">
+                        <div class="form-floating mt-2 mb-3">
+                            <input type="text" id="inputname" name="name" class="form-control" id="floatingInput"
+                                value="">
+                            <label for="floatingInput">Tên sản phẩm</label>
+                        </div>
+                        <div class="form-floating mt-4">
+                            <input type="text" id="inputdescription" name="description" class="form-control"
+                                id="floatingInput" value="">
+                            <label for="floatingInput">Mô tả</label>
+                        </div>
+                        <div class="form-floating mt-4">
+                            <input type="text" id="inputprice" name="price" class="form-control" id="floatingInput"
+                                value="">
+                            <label for="floatingInput">Giá</label>
+                        </div>
+                        <div class="mt-4">
+                            <label for="disabledSelect">Thể loại</label>
+                            <select id="disabledSelect" class="form-select">
+                                <option>Gà rán</option>
+                                <option>Pizza</option>
+                                <option>đồ ăn vặt</option>
+                                <option>Đồ uống</option>
+                            </select>
+                        </div>
+                        <div class="mt-4">
+                            <label for="file">Thêm ảnh</label>
+                            <input type="file" name="myFile" class="form-control " id="file">
+                        </div>
+                        <input type="submit" name="submit" value="Lưu sản phẩm"
+                            class="submit_product fs-3 pe-3 ps-3 mt-5 ms-5 pt-1 pb-1 rounded-pill border border-light"
+                            style="background-color:rgb(255, 145, 0)">
+                    </form>
+                </div>
+            </div>
         </div>
     </main>
-
-
-
-
-
-
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+    <!-- footer -->
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
         crossorigin="anonymous"></script>
 </body>
